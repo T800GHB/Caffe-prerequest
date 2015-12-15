4082ba4d78cfc08408d290a10f28f9941a79ee74
Hanbing Guo <t800ghb@qq.com>
2015-12-15 21:44:58 +0800

    First edition

---
+++ caffe_prerequest.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+APT_FLAGS="-y"
+
+echo
+echo Installing apt-get packages ...
+echo
+
+sudo apt-get update
+
+# Caffe base
+sudo apt-get install $APT_FLAGS libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler libatlas-base-dev
+sudo apt-get install $APT_FLAGS --no-install-recommends libboost-all-dev
+
+# Caffe python
+sudo apt-get install $APT_FLAGS python-dev python-pip gfortran
+
+# Caffe pip
+sudo apt-get install $APT_FLAGS cython python-numpy python-scipy python-skimage python-matplotlib python-h5py python-leveldb python-networkx python-pandas python-dateutil python-protobuf python-gflags python-yaml python-pil
+
+#Remind
+echo Please install CUDA and cudnn by NVIDIA offical package.
+echo Please do not forget to add path to config file.
+echo Add /usr/local/cuda/lib64 and /lib to /etc/ld.so.conf.d/cuda.conf
+echo To make it work use :sudo ldconfig -v
+echo
+echo Add HDF5 Path to Makefile.config
+echo INCLUDE_DIRS := $(PYTHON_INCLUDE) /usr/local/include /usr/lib/x86_64-linux-gnu/hdf5/serial/include
+echo LIBRARY_DIRS := $(PYTHON_LIB) /usr/local/lib /usr/lib /usr/lib/x86_64-linux-gnu/hdf5/serial
+echo
